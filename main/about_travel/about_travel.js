c47ca57ad98465c70e712a43ee16851158ca392c
Twoia-Kotletochka <olenchenko.ilya@ukr.net>
2024-03-26 14:53:54 +0200

    about_travel +

--- main/about_travel/about_travel.js
+++ main/about_travel/about_travel.js
@@ -1,11 +1,14 @@
 // Get the buttons by their IDs
-const locationBtn = document.getElementById('location-btn');
 const ticketNumberBtn = document.getElementById('ticket-number-btn');
+const locationBtn = document.getElementById('location-btn');
+
+const flightnumber = document.getElementById('flight-number'); //ticket-number-btn
+const destination = document.getElementById('destination');
 
 // Function to remove active class from all buttons
 function deactivateAllButtons() {
-  locationBtn.classList.remove('active');
   ticketNumberBtn.classList.remove('active');
+  locationBtn.classList.remove('active');
 }
 
 // Function to activate a button
@@ -17,9 +20,13 @@ function activateButton(btn) {
 // Add click event listener to the location button
 locationBtn.addEventListener('click', function() {
   activateButton(locationBtn);
+  destination.style.display = 'none';
+  flightnumber.style.display = 'block';
 });
 
 // Add click event listener to the ticket number button
 ticketNumberBtn.addEventListener('click', function() {
   activateButton(ticketNumberBtn);
+  flightnumber.style.display = 'none';
+  destination.style.display = 'block';
 });
