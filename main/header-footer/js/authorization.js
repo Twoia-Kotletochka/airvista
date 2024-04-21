9f0d54fd808c83c36760238914aa4a538381b0a3
Twoia-Kotletochka <olenchenko.ilya@ukr.net>
2024-04-21 20:38:21 +0300

    save form

---
+++ main/header-footer/js/authorization.js
@@ -0,0 +1,19 @@
+document.getElementById("showForm").addEventListener("click", function() {
+  document.getElementById("myForm").style.display = "block";
+  document.querySelector(".overlay").style.display = "block";
+  setTimeout(function() {
+    document.getElementById("myForm").style.opacity = 1;
+    document.getElementById("myForm").style.transform = "translate(-50%, 50%)";
+  }, 100);
+  document.body.style.overflow = 'hidden'; // блокировка скролла
+});
+
+document.getElementById("closeForm").addEventListener("click", function() {
+  document.getElementById("myForm").style.opacity = 0;
+  document.getElementById("myForm").style.transform = "translate(-50%, 100%)";
+  setTimeout(function() {
+    document.getElementById("myForm").style.display = "none";
+    document.querySelector(".overlay").style.display = "none";
+    document.body.style.overflow = 'auto'; // разблокировка скролла
+  }, 500);
+});
