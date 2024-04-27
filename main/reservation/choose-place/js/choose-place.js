f685e4e23dc60a5d66e696b1843ad2fe1205cf86
Twoia-Kotletochka <olenchenko.ilya@ukr.net>
2024-04-27 19:37:04 +0300

    save

--- main/reservation/choose-place/js/choose-place.js
+++ main/reservation/choose-place/js/choose-place.js
@@ -19,3 +19,45 @@ function toggleActive(id) {
         activeDivId = id;
     }
 }
+
+
+let selectedArea = null;
+let selectedTicket = null;
+
+function selectTicket(ticket) {
+    const ticketNumber = ticket.getAttribute('data-ticket');
+    const ticketPrice = ticket.getAttribute('data-price');
+    const ticketName = ticket.innerText;
+    selectedTicket = ticket;
+
+    if (selectedArea) {
+        document.getElementById(`ticketName${selectedArea}`).innerText = `Выбран билет ${ticketNumber}: ${ticketName}`;
+        document.getElementById(`ticketPrice${selectedArea}`).innerText = `Цена: ${ticketPrice} грн`;
+        ticket.classList.add('selected');
+    } else {
+        alert('Выберите область для размещения номера билета');
+    }
+}
+
+function selectArea(area) {
+    if (selectedArea === area) {
+        document.getElementById(`area${area}`).classList.remove('selected');
+        document.getElementById(`ticketName${area}`).innerText = '';
+        document.getElementById(`ticketPrice${area}`).innerText = '';
+        selectedArea = null;
+        if (selectedTicket) {
+            selectedTicket.classList.remove('selected');
+            selectedTicket = null;
+        }
+    } else {
+        document.getElementById(`area${area}`).classList.add('selected');
+        selectedArea = area;
+        if (selectedTicket) {
+            const ticketNumber = selectedTicket.getAttribute('data-ticket');
+            const ticketName = selectedTicket.innerText;
+            const ticketPrice = selectedTicket.getAttribute('data-price');
+            document.getElementById(`ticketName${area}`).innerText = `Выбран билет ${ticketNumber}: ${ticketName}`;
+            document.getElementById(`ticketPrice${area}`).innerText = `Цена: ${ticketPrice} грн`;
+        }
+    }
+}
